d371ca81aa1eeb2bbe953ae4511c007ba693b653
Yana Kozhokar <yanakozhokar1996@gmail.com>
2022-12-23 13:15:31 +0200

    task-02 completed

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const elements = ingredients.map((el) => {
+  const addingEl = document.createElement("li");
+  addingEl.textContent = el;
+  addingEl.classList.add("item");
+  return addingEl;
+});
+
+document.querySelector("#ingredients").append(...elements);
