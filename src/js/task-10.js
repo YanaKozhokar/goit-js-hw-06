084d51aaebb9bd4e0d9b89818ea0e18fbcbcac0a
Yana Kozhokar <yanakozhokar1996@gmail.com>
2022-12-24 22:56:38 +0200

    task-10 updated

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,25 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const divBoxesRef = document.querySelector("#boxes");
+const inputRef = document.querySelector("input[type='number']");
+const btnCreateClickHandler = document.querySelector("button[data-create]");
+const btnDestroyClickHandler = document.querySelector("button[data-destroy]");
+let boxesArray = [];
+
+function createBoxes(amount) {
+  let width = 30;
+  let height = 30;
+  for (let i = 0; i < amount; i += 1) {
+    boxesArray.push(
+      `<div width="${width}" heigth="${height}" style="background-color: ${getRandomHexColor()}"></div>`
+    );
+    width += 10;
+    height += 10;
+  }
+}
+
+btnCreateClickHandler.addEventListener("click", () => {
+  createBoxes(inputRef.value);
+});
