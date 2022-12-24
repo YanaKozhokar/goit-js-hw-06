4e13bae766c2486dd97f3bfa0a2dbbdc52241778
Yana Kozhokar <yanakozhokar1996@gmail.com>
2022-12-24 22:10:27 +0200

    task-09 completed

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyRef = document.querySelector("body");
+const textRef = document.querySelector(".color");
+
+const buttonHandle = document.querySelector(".change-color");
+buttonHandle.addEventListener("click", () => {
+  const color = getRandomHexColor();
+  bodyRef.style = `background-color: ${color}`;
+  textRef.textContent = color;
+});
