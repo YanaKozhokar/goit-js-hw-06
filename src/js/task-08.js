37385bb825cdad5c5d1289e9e441231e49a75f12
Yana Kozhokar <yanakozhokar1996@gmail.com>
2022-12-24 13:47:38 +0200

    task-08 updated

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,17 @@
+const formInputHandler = document.querySelector(".login-form");
+const inputFormRef = document.querySelectorAll("form input");
+const submitBtnRef = document.querySelector("button[type='submit']");
 
+formInputHandler.addEventListener("submit", (event) => {
+  event.preventDefault();
+  try {
+    inputFormRef.forEach((el) => {
+      if (el.value === "") {
+        throw "All fildes must be filled!";
+      } else {
+      }
+    });
+  } catch (exception) {
+    alert(exception);
+  }
+});
