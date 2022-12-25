af43e7639f63efbd6f0bc948b4958634a3bd654d
Yana Kozhokar <yanakozhokar1996@gmail.com>
2022-12-25 12:22:32 +0200

    task-10 updated

--- js/task-10.js
+++ js/task-10.js
@@ -6,9 +6,9 @@ const divBoxesRef = document.querySelector("#boxes");
 const inputRef = document.querySelector("input[type='number']");
 const btnCreateClickHandler = document.querySelector("button[data-create]");
 const btnDestroyClickHandler = document.querySelector("button[data-destroy]");
-let boxesArray = [];
 
 function createBoxes(amount) {
+  const boxesArray = [];
   let width = 30;
   let height = 30;
   for (let i = 0; i < amount; i += 1) {
@@ -18,13 +18,15 @@ function createBoxes(amount) {
     width += 10;
     height += 10;
   }
+  return boxesArray;
 }
 
 btnCreateClickHandler.addEventListener("click", () => {
-  createBoxes(inputRef.value);
   divBoxesRef.insertAdjacentHTML(
     "beforeend",
-    boxesArray.map((el) => el).join("")
+    createBoxes(inputRef.value)
+      .map((el) => el)
+      .join("")
   );
 });
 
