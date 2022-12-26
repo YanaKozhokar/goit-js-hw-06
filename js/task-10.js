532876dc901014770ce29baa6a1c2958882bd31c
Yana Kozhokar <yanakozhokar1996@gmail.com>
2022-12-26 12:41:28 +0200

    task-10 updated

--- js/task-10.js
+++ js/task-10.js
@@ -6,11 +6,11 @@ const divBoxesRef = document.querySelector("#boxes");
 const inputRef = document.querySelector("input[type='number']");
 const btnCreateClickHandler = document.querySelector("button[data-create]");
 const btnDestroyClickHandler = document.querySelector("button[data-destroy]");
+let width = 30;
+let height = 30;
 
 function createBoxes(amount) {
   const boxesArray = [];
-  let width = 30;
-  let height = 30;
   for (let i = 0; i < amount; i += 1) {
     boxesArray.push(
       `<div style="background-color: ${getRandomHexColor()};  width: ${width}px; height: ${height}px;"></div>`
@@ -33,5 +33,7 @@ btnCreateClickHandler.addEventListener("click", () => {
 btnDestroyClickHandler.addEventListener("click", () => {
   document.querySelectorAll("#boxes > div").forEach((el) => {
     el.remove();
+    width = 30;
+    height = 30;
   });
 });
